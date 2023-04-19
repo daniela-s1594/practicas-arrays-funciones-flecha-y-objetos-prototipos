923350e5ce15bba342947eaebfc102d791ce4a55
daniela <danielamvirviescas2004@gmail.com>
2023-04-18 19:46:57 -0500

    Creacion de los archivos arrays y objetos en js.DMV

---
+++ arrays.js
@@ -0,0 +1,8 @@
+let fruits =["Apple","Orange","Plum"];
+console.log(fruits[1]);
+fruits[0]="Pear";
+console.log(fruits);
+
+console.log(fruits.length);
+fruits[fruits.length]="Grape";
+fruits.push("Banana");
